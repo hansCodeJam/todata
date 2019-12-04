afcfcc37329638cd3a5bd0a04336d527abbd7435
Hans Vos <hans.vos@codeimmersives.com>
2019-12-04 15:47:27 -0500

    added object and query selector

--- add-todo.js
+++ add-todo.js
@@ -1,5 +1,17 @@
-
 // A function that adds an item to our todo list.
-const addTodo = function(todo) {
+const addTodo = function(text) {
+  let todo = {
+    text: text,
+    id: todos.length + 1,
+    complete: false,
+    priority: 1
+  }
   todos.push(todo);
 }
+
+const input = document.querySelector('.todo-input');
+
+document.querySelector('.add-todo').addEventListener('click', function(){
+  addTodo(input.value);
+  input.value = ''
+})
